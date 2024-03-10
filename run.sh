fc064a9ac2bd92131dde87f43f25926e957cc175
Romain Aubry <romain.m.aubry@gmail.com>
2024-03-10 00:34:13 +0000

    Fix bug

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
  make clean build
  make  run
  make  run ARGS="--input=data/casablanca.pgm"
- make  run ARGS="--input=data/monalisa.pgm"
+ make  run ARGS="--input=data/mona_lisa.pgm"
  make  run ARGS="--input=data/baboon.pgm"
